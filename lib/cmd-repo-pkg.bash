0810ab1c5af671dd51989fa45a201a8496cef8a8
TekWize.ly <TekWize.ly@gmail.com>
2021-08-09 15:54:08 -0700

    fix ./... usage in cmd-repo-pkg

--- lib/cmd-repo-pkg.bash
+++ lib/cmd-repo-pkg.bash
@@ -4,13 +4,16 @@
 
 prepare_repo_hook_cmd "$@"
 
+if [ "${use_dot_dot_dot:-}" -eq 1 ]; then
+	OPTIONS+=('./...')
+fi
 export GO111MODULE=off
 if [ "${error_on_output:-}" -eq 1 ]; then
-	output=$("${cmd[@]}" "${OPTIONS[@]}" ./... 2>&1)
+	output=$("${cmd[@]}" "${OPTIONS[@]}" 2>&1)
 	if [ -n "${output}" ]; then
 		printf "%s\n" "${output}"
 		exit 1
 	fi
 else
-	"${cmd[@]}" "${OPTIONS[@]}" ./...
+	"${cmd[@]}" "${OPTIONS[@]}"
 fi
