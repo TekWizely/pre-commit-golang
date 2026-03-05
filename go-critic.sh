b7cb3954a2a732e8e4995e762f9273f2bc75e865
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2026-03-05 09:45:18 -0500

    Support both possible go-critic binary names
    Even though go-critic is moving to name its binary `go-critic` it will
    still install as `gocritic` if you do not specifically install the
    `go-critic` command.

--- go-critic.sh
+++ go-critic.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-cmd=(go-critic check)
+# Use legacy binary name if it exists; otherwise use the new binary name.
+if command -v gocritic >/dev/null 2>&1; then
+  binary_name="gocritic"
+else
+  binary_name="go-critic"
+fi
+
+cmd=("$binary_name" check)
 . "$(dirname "${0}")/lib/cmd-files.bash"
